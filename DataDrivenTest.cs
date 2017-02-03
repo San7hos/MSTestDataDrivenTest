bdb7472bc3f3c2229ad2be32b8fb7898d3d3132e
Michal Stasa <michal.stasa@gmail.com>
2017-02-03 10:40:03 +0100

    Implement another ways to add test cases

--- DataDrivenTest.cs
+++ DataDrivenTest.cs
@@ -1,4 +1,10 @@
-﻿namespace Santhos.MSTest
+﻿//-------------------------------------------------------------------------------------
+//<copyright file="DataDrivenTest.cs" company="Santhos.net">
+//     Copyright(c) Santhos.net | All rights reserved
+//</copyright>
+//-------------------------------------------------------------------------------------
+
+namespace Santhos.MSTest
 {
     using System;
     using System.Collections.Generic;
@@ -15,6 +21,22 @@
         private readonly List<object[]> testCases = new List<object[]>();
 
         private int testCaseArgumentCount = 0;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataDrivenTest"/> class.
+        /// </summary>
+        public DataDrivenTest()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataDrivenTest"/> class.
+        /// </summary>
+        /// <param name="testCaseSet">Set of initial test cases</param>
+        public DataDrivenTest(IEnumerable<IEnumerable<object>> testCaseSet)
+        {
+            this.ArrangeTestCases(testCaseSet);
+        }
 
         /// <summary>
         /// Arranges a test case with given arguments
@@ -47,6 +69,41 @@
         }
 
         /// <summary>
+        /// Arranges a test case with given arguments
+        /// </summary>
+        /// <param name="testCase">Test case arguments (aka the test case)</param>
+        /// <returns>Fluent API</returns>
+        public DataDrivenTest Arrange(IEnumerable<object> testCase)
+        {
+            return this.Arrange(testCase.ToArray());
+        }
+
+        /// <summary>
+        /// Arranges a test case with result of the given function
+        /// </summary>
+        /// <param name="testCase">Test case function that returns the test case (arguments)</param>
+        /// <returns>Fluent API</returns>
+        public DataDrivenTest Arrange(Func<IEnumerable<object>> testCase)
+        {
+            return this.Arrange(testCase());
+        }
+
+        /// <summary>
+        /// Arranges multiple test cases at once
+        /// </summary>
+        /// <param name="testCaseSet">Set of test cases</param>
+        /// <returns>FluentAPI</returns>
+        public DataDrivenTest ArrangeTestCases(IEnumerable<IEnumerable<object>> testCaseSet)
+        {
+            foreach (var testCase in testCaseSet)
+            {
+                this.Arrange(testCase);
+            }
+
+            return this;
+        }
+
+        /// <summary>
         /// Calls actAndAssert method for each previously given test case
         /// </summary>
         /// <param name="actAndAssert">Method that should act on and assert each test case</param>
@@ -72,6 +129,8 @@
                 }
             }
         }
+
+        #region Typed ActAndAssert
 
         /// <summary>
         /// Calls actAndAssert method for each previously given test case
@@ -117,5 +176,52 @@
         {
             this.ActAndAssert((Delegate)actAndAssert);
         }
+
+        /// <summary>
+        /// Calls actAndAssert method for each previously given test case
+        /// </summary>
+        /// <param name="actAndAssert">Method that should act on and assert each test case</param>
+        public void ActAndAssert<T1, T2, T3, T4, T5, T6>(Action<T1, T2, T3, T4, T5, T6> actAndAssert)
+        {
+            this.ActAndAssert((Delegate)actAndAssert);
+        }
+
+        /// <summary>
+        /// Calls actAndAssert method for each previously given test case
+        /// </summary>
+        /// <param name="actAndAssert">Method that should act on and assert each test case</param>
+        public void ActAndAssert<T1, T2, T3, T4, T5, T6, T7>(Action<T1, T2, T3, T4, T5, T6, T7> actAndAssert)
+        {
+            this.ActAndAssert((Delegate)actAndAssert);
+        }
+
+        /// <summary>
+        /// Calls actAndAssert method for each previously given test case
+        /// </summary>
+        /// <param name="actAndAssert">Method that should act on and assert each test case</param>
+        public void ActAndAssert<T1, T2, T3, T4, T5, T6, T7, T8>(Action<T1, T2, T3, T4, T5, T6, T7, T8> actAndAssert)
+        {
+            this.ActAndAssert((Delegate)actAndAssert);
+        }
+
+        /// <summary>
+        /// Calls actAndAssert method for each previously given test case
+        /// </summary>
+        /// <param name="actAndAssert">Method that should act on and assert each test case</param>
+        public void ActAndAssert<T1, T2, T3, T4, T5, T6, T7, T8, T9>(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9> actAndAssert)
+        {
+            this.ActAndAssert((Delegate)actAndAssert);
+        }
+
+        /// <summary>
+        /// Calls actAndAssert method for each previously given test case
+        /// </summary>
+        /// <param name="actAndAssert">Method that should act on and assert each test case</param>
+        public void ActAndAssert<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10>(Action<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> actAndAssert)
+        {
+            this.ActAndAssert((Delegate)actAndAssert);
+        }
+
+        #endregion
     }
 }
